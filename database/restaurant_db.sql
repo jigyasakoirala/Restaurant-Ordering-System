fbeb815522f35c95ab504f71c938b70cd5d6aec8
jigyasakoirala <166095190+jigyasakoirala@users.noreply.github.com>
2026-07-11 14:26:03 +0545

    Added working restaurant database

---
+++ database/restaurant_db.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2026 at 10:32 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurant_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table restaurant_db.admin: #1932 - Table 'restaurant_db.admin' doesn't exist in engine
+-- Error reading data for table restaurant_db.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `restaurant_db`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table restaurant_db.categories: #1932 - Table 'restaurant_db.categories' doesn't exist in engine
+-- Error reading data for table restaurant_db.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `restaurant_db`.`categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foods`
+--
+-- Error reading structure for table restaurant_db.foods: #1932 - Table 'restaurant_db.foods' doesn't exist in engine
+-- Error reading data for table restaurant_db.foods: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `restaurant_db`.`foods`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table restaurant_db.orders: #1932 - Table 'restaurant_db.orders' doesn't exist in engine
+-- Error reading data for table restaurant_db.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `restaurant_db`.`orders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+-- Error reading structure for table restaurant_db.order_items: #1932 - Table 'restaurant_db.order_items' doesn't exist in engine
+-- Error reading data for table restaurant_db.order_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `restaurant_db`.`order_items`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
