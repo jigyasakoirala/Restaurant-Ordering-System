7cb94b4e0607393af3a02c3b2e2c15ec791995fd
jigyasakoirala <166095190+jigyasakoirala@users.noreply.github.com>
2026-07-11 22:14:49 +0545

    Added automatic database configuration

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,27 @@
 <?php
-$servername = "sql107.infinityfree.com";
-$username = "if0_42380762";
-$password = "71ICQA3wLbdOS";
-$database = "if0_42380762_restaurant_db";
+
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+
+    // Localhost Database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "restaurant_db";
+
+} else {
+
+    // InfinityFree Database
+    $servername = "sql107.infinityfree.com";
+    $username = "if0_42380762";
+    $password = "71ICQA3wLbdOS";
+    $database = "if0_42380762_restaurant_db";
+
+}
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 if (!$conn) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
+
 ?>
